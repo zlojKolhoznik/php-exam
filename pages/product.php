1662fd06e4c25075e817d6e0cb9e0807a1266a66
Roman Portianko <rportyanko10@gmail.com>
2023-05-03 12:15:48 +0300

    Added bootstrap JS-script to product.php

--- pages/product.php
+++ pages/product.php
@@ -62,5 +62,6 @@
                 </div>
             </div>
         </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
