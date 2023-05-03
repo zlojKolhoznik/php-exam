a291f74a76afcc746bf6686e11f935ff5c02334e
Roman Portianko <rportyanko10@gmail.com>
2023-05-03 09:59:07 +0300

    Made contacts page

---
+++ pages/contacts.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel='stylesheet' href='../css/bootstrap.css'>
+        <link rel='stylesheet' href='../css/custom-styles.css'>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+        <title>Contacts - ShopyShop</title>
+    </head>
+    <body>
+        <?php include_once '../includes/navbar.php'; ?>
+        <div class="container">
+            <!--Dummy contacts-->
+            <div class="container py-5">
+                <h1 class="text-center mb-4">ShopyShop Contacts</h1>
+                <div class="row">
+                    <div class="col-md-6 mb-4">
+                        <h2 class="mb-3">Address</h2>
+                        <p>Main Street, 123</p>
+                        <p>Anytown, Ukraine, 09352</p>
+                    </div>
+                    <div class="col-md-6 mb-4">
+                        <h2 class="mb-3">Phone Number</h2>
+                        <p>+380(97)-555-342-4534</p>
+                        <h2 class="mb-3">Sales center</h2>
+                        <p>+380(96)-555-423-5243</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 mb-4">
+                        <h2 class="mb-3">Email Address</h2>
+                        <p>info@example.com</p>
+                    </div>
+                    <div class="col-md-6 mb-4">
+                        <h2 class="mb-3">Social Media</h2>
+                        <a href="#" class="btn btn-primary mb-2"><i class="bi bi-facebook me-2"></i> Facebook</a>
+                        <a href="#" class="btn btn-primary mb-2"><i class="bi bi-twitter me-2"></i> Twitter</a>
+                        <a href="#" class="btn btn-primary mb-2"><i class="bi bi-instagram me-2"></i> Instagram</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
