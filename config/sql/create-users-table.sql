95f1f4d88ef6eea86cf5d3ade2cb60be612a773c
Roman Portianko <rportyanko10@gmail.com>
2023-05-03 12:48:17 +0300

    Seeded admin account

--- config/sql/create-users-table.sql
+++ config/sql/create-users-table.sql
@@ -10,4 +10,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `phone` (`phone`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO `users` (`id`, `name`, `email`, `phone`, `password_hash`, `role`) VALUES
+(1, 'Admin', 'root@shopyshop.com', '+380970043543', '4813494d137e1631bba301d5acab6e7bb7aa74ce1185d456565ef51d737677b2', 1);
 COMMIT;
